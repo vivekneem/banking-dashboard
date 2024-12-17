53ba104a6332950cd2e93bac93b20c040cfd9e44
Vivek <vivekdesai@neemconsulting.com>
2024-12-17 15:54:59 +0530

    feat: added all dashboard page components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -29,9 +29,7 @@ const Dashboard: React.FC = () => {
   return (
     <div className="">
       <div className="grid grid-cols-12 gap-[30px]">
-        {/* Left Column - My Cards & Weekly Activity */}
         <div className="col-span-8">
-          {/* My Cards Section */}
           <div className="w-[730px] h-[282px] mb-[30px]">
             <Card title="My Cards" showSeeAll>
               <div className="grid grid-cols-2 gap-4">
@@ -42,36 +40,29 @@ const Dashboard: React.FC = () => {
             </Card>
           </div>
 
-          {/* Weekly Activity */}
           <div className="w-[730px] h-[367px]">
             <WeeklyActivity />
           </div>
         </div>
 
-        {/* Right Column - Recent Transactions & Expense Statistics */}
         <div className="col-span-4">
-          {/* Recent Transactions */}
           <div className="w-[350px] h-[282px] mb-[30px]">
             <RecentTransactions />
           </div>
 
-          {/* Expense Statistics */}
           <div className="w-[350px] h-[367px]">
             <ExpenseStatistics />
           </div>
         </div>
       </div>
 
-      {/* Bottom Row */}
       <div className="grid grid-cols-12 gap-[30px] mt-[30px]">
-        {/* Quick Transfer */}
         <div className="col-span-5">
           <div className="w-[445px] h-[323px]">
             <QuickTransfer />
           </div>
         </div>
 
-        {/* Balance History */}
         <div className="col-span-7">
           <div className="w-[635px] h-[323px]">
             <BalanceHistory />
